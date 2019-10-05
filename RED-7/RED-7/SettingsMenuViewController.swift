239dbf26db7e6673d993824a4035fdc1a77b3b4d
perveevm <perveev_m@mail.ru>
2019-10-05 22:54:38 +0300

    Settings screen added

---
+++ RED-7/RED-7/SettingsMenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsMenuViewController.swift
+//  RED-7
+//
+//  Created by Михаил Первеев on 05/10/2019.
+//  Copyright © 2019 com.perveev.mike. All rights reserved.
+//
+
+import UIKit
+
+class SettingsMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
