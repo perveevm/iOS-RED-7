6e550fbface1e08d02c6195ac9af2a4d63a903cb
perveevm <perveev_m@mail.ru>
2019-12-18 00:17:46 +0300

    Some layout tests

---
+++ RED-7/RED-7/GameSceneViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameSceneViewController.swift
+//  RED-7
+//
+//  Created by Михаил Первеев on 17.12.2019.
+//  Copyright © 2019 com.perveev.mike. All rights reserved.
+//
+
+import UIKit
+
+class GameSceneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
